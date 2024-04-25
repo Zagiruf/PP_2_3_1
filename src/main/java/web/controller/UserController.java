fe46c8af38402ba2697ad96c6060676f7294d92e
Zagiruf <Zagiruf@gmail.com>
2024-04-25 18:14:10 +0500

    c

--- src/main/java/web/controller/UserController.java
+++ src/main/java/web/controller/UserController.java
@@ -4,27 +4,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import web.model.Car;
-import web.service.CarService;
-import web.service.CarServiceImpl;
-
-import java.util.ArrayList;
-import java.util.List;
+import web.service.UserService;
 
 @Controller
-@RequestMapping("/cars")
-public class CarController {
-    private final CarService carService;
+@RequestMapping("/user")
+public class UserController {
+    private final UserService userService;
     @Autowired
-    public CarController(CarService carService) {
-        this.carService = carService;
+    public UserController(UserService user) {
+        this.userService = user;
     }
     @GetMapping()
     public String show(@RequestParam(value = "count", defaultValue = "5") int count, Model model) {
-        model.addAttribute("cars", carService.show(count));
-        return "cars";
+        model.addAttribute("cars", userService.show(count));
+        return "users";
     }
 }
